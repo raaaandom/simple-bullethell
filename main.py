84ff7cebf10fddee78dbebf8a6726f328a41214f
raaaandom <itzraandom@gmail.com>
2023-02-09 20:22:53 +0100

    added basic powerup (points)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#TODO: basic powerup (points), collision fix on high pixel per sec
+#TODO: basic powerup (points)
+#TODO: collision fix on high pixel per sec
 
 #Libraries
 import pygame, time
@@ -31,26 +32,40 @@ WINDOW = pygame.display.set_mode(WINDOW_SIZE, pygame.NOFRAME)
 pygame.display.set_caption(WINDOW_CAPTION)
 pygame.display.set_icon(WINDOW_ICON)
 
+#Powerup values
+POWERUP_POINT = 1
+POWERUP_WEAPON = 2
+POWERUP_CHARGE = 3
+
+# in game points var (increments with point powerup)
+points = 0
+
 #Object default values
 DEFAULT_X = 0
 DEFAULT_Y = 0
 DEFAULT_Z = 0
-DEFAULT_ON = False
+DEFAULT_ON = True
 DEFAULT_TEXTURE_ID = 0
-DEFAULT_FREE = True
+DEFAULT_FREE = False
 DEFAULT_KEYCONTROL = False
 DEFAULT_COLLIDE = False
+DEFAULT_POWERUP = None
+DEFAULT_POWERUP_AMOUNT = 0
+DEFAULT_POWERUP_PICKUP = False
 
 #Object arrays
 OBJECT_COUNT_MAX = 500
 x = [DEFAULT_X] * OBJECT_COUNT_MAX                                  # x pos
 y = [DEFAULT_Y] * OBJECT_COUNT_MAX                                  # y pos
 z = [DEFAULT_Z] * OBJECT_COUNT_MAX                                  # z layer
-on = [DEFAULT_ON] * OBJECT_COUNT_MAX                                # need to render?
+on = [False] * OBJECT_COUNT_MAX                                     # need to render?
 texture_id = [DEFAULT_TEXTURE_ID] * OBJECT_COUNT_MAX                # texture array index
-free = [DEFAULT_FREE] * OBJECT_COUNT_MAX                            # can be overwrited?
+free = [True] * OBJECT_COUNT_MAX                                    # can be overwrited?
 keycontrol = [DEFAULT_KEYCONTROL] * OBJECT_COUNT_MAX                # moved by keyboard?
 collide = [DEFAULT_COLLIDE] * OBJECT_COUNT_MAX                      # collision enabled?
+powerup = [DEFAULT_POWERUP] * OBJECT_COUNT_MAX                      # powerup type (point, charge ...) 
+powerup_amount = [DEFAULT_POWERUP_AMOUNT] * OBJECT_COUNT_MAX        # powerup value (tot points, charge...)
+powerup_pickup = [DEFAULT_POWERUP_PICKUP] * OBJECT_COUNT_MAX        # should pickup powerups?
 
 #Check collision between two objects
 def checkCollision(obj1, obj2, offx=None, offy=None):
@@ -72,6 +87,10 @@ def freeObjectID():
         if free[obj] == True:
             return obj
 
+def deleteObject(obj):
+    on[obj] = False
+    free[obj] = True
+
 #Generate an object
 def createObject(
                     _x = DEFAULT_X,
@@ -81,7 +100,10 @@ def createObject(
                     _texture_id = DEFAULT_TEXTURE_ID,
                     _free = DEFAULT_FREE,
                     _keycontrol = DEFAULT_KEYCONTROL,
-                    _collide = DEFAULT_COLLIDE
+                    _collide = DEFAULT_COLLIDE,
+                    _powerup = DEFAULT_POWERUP,
+                    _powerup_amount = DEFAULT_POWERUP_AMOUNT,
+                    _powerup_pickup = DEFAULT_POWERUP_PICKUP
                 ):
     id = freeObjectID()
     x[id] = _x
@@ -92,6 +114,9 @@ def createObject(
     free[id] = _free
     keycontrol[id] = _keycontrol
     collide[id] = _collide
+    powerup[id] = _powerup
+    powerup_amount[id] = _powerup_amount
+    powerup_pickup[id] = _powerup_pickup
 
 #Z Layers
 Z_LAYER_COUNT = 10
@@ -101,23 +126,56 @@ TEXTURE_COUNT = 500
 ID_TEXTURE_DEFAULT = 0
 ID_TEXTURE_PLAYER = 1
 ID_TEXTURE_INGAMEUIBG = 2
+ID_TEXTURE_POINT = 3
+ID_TEXTURE_WEAPON = 4
+ID_TEXTURE_CHARGE = 5
 
 #Texture array
 texture = [None] * TEXTURE_COUNT
 texture[ID_TEXTURE_DEFAULT] = pygame.image.load("data/textures/default.png")
 texture[ID_TEXTURE_PLAYER] = pygame.image.load("data/textures/player.png")
 texture[ID_TEXTURE_INGAMEUIBG] = pygame.image.load("data/textures/ingame_ui_bg.png")
+texture[ID_TEXTURE_POINT] = pygame.image.load("data/textures/point.png")
+texture[ID_TEXTURE_WEAPON] = pygame.image.load("data/textures/weapon.png")
+texture[ID_TEXTURE_CHARGE] = pygame.image.load("data/textures/charge.png")
 
 #Collision Mask array (IDs same as textures)
 mask = [None] * TEXTURE_COUNT
 mask[ID_TEXTURE_PLAYER] = pygame.mask.from_surface(texture[ID_TEXTURE_PLAYER])
 mask[ID_TEXTURE_INGAMEUIBG] = pygame.mask.from_surface(texture[ID_TEXTURE_INGAMEUIBG])
+mask[ID_TEXTURE_POINT] = pygame.mask.from_surface(texture[ID_TEXTURE_POINT])
+mask[ID_TEXTURE_WEAPON] = pygame.mask.from_surface(texture[ID_TEXTURE_WEAPON])
+mask[ID_TEXTURE_CHARGE] = pygame.mask.from_surface(texture[ID_TEXTURE_CHARGE])
 
-#Create the player [DEBUG]
-createObject(800,400,0,True,1,False,True,True)
+#Create the player
+createObject(
+_x=800,
+_y=400,
+_z=4,
+_texture_id=ID_TEXTURE_PLAYER,
+_keycontrol=True,
+_collide=True,
+_powerup_pickup=True
+)
 
 #Create the ingame ui bg
-createObject(0,0,9,True,ID_TEXTURE_INGAMEUIBG,False,False,True)
+createObject(
+_x=0,
+_y=0,
+_z=9,
+_texture_id=ID_TEXTURE_INGAMEUIBG,
+_collide=True
+)
+
+#Create powerup [debug]
+createObject(
+_x=600,
+_y=300,
+_z=3,
+_texture_id=ID_TEXTURE_POINT,
+_powerup=POWERUP_POINT,
+_powerup_amount=1
+)
 
 # !!! NEEDS TO BE LAST INIT CALL !!!
 clockfix_now = 0
@@ -196,7 +254,26 @@ while running_flag:
                 x[obj] = next_x
             if not collision_flag_y:
                 y[obj] = next_y
-    
+
+    #Manage powerups
+    for obj1 in range(OBJECT_COUNT_MAX):
+        if powerup[obj1] != None: # if it is a powerup
+
+            if on[obj1]: # only on rendered powerups
+
+                for obj2 in range(OBJECT_COUNT_MAX):
+                    if powerup_pickup[obj2]: # get pickup obj
+
+                        #check collision to see if picked up
+                        if checkCollision(obj1, obj2):
+
+                            #POINTS POWERUP
+                            if powerup[obj1] == POWERUP_POINT:
+                                points += powerup_amount[obj1]
+                            
+                            #CLEAR POWERUP
+                            deleteObject(obj1)
+
     #Render system
     WINDOW.fill(WINDOW_FILL_COLOR)  # clear
     for _z in range(Z_LAYER_COUNT): # sort z layer
@@ -205,5 +282,6 @@ while running_flag:
                 if on[obj]:                         # if on
                     WINDOW.blit(texture[texture_id[obj]], (x[obj], y[obj])) # render it
     pygame.display.flip()   # update
-    
-    
+
+#On game close, call pygame.quit
+pygame.quit()
